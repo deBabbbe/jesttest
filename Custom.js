1b5ccb515198690eb07784ce0c36cd6c429f2e07
deBabbbe <neotbh@gmx.de>
2022-03-08 14:17:54 +0100

    Tree tested till 4 lines

--- Custom.js
+++ Custom.js
@@ -2,4 +2,20 @@ function sum(a, b) {
   return a + b;
 }
 
-module.exports = { sum };
+function tree(sign, size) {
+  let result = Array(size)
+    .fill()
+    .map((_) => "");
+  for (let line = 0; line < size; line++) {
+    const spaces = size - 1 - line;
+    result[line] = result[line].padStart(spaces, " ");
+    const signCount = line == 0 ? 1 : 1 + line + line;
+    for (let x = 0; x < signCount; x++) {
+      result[line] += sign;
+    }
+    result[line] = result[line].padEnd(result[line].length + spaces, " ");
+  }
+  return result.join("\n");
+}
+
+module.exports = { sum, tree };
--- Custom.test.js
+++ Custom.test.js
@@ -1,4 +1,4 @@
-const { sum } = require("./Custom");
+const { sum, tree } = require("./Custom");
 const each = require("jest-each").default;
 
 describe("sum", () => {
@@ -7,5 +7,27 @@ describe("sum", () => {
     [2, 2, 4],
   ]).test("returns the result of adding %d to %d'", (a, b, res) => {
     expect(sum(a, b)).toBe(res);
+  });
+});
+
+describe("Tree", () => {
+  it("1 line", () => {
+    const expected = "*";
+    expect(tree("*", 1)).toEqual(expected);
+  });
+
+  it("2 lines", () => {
+    const expected = " * \n" + "***";
+    expect(tree("*", 2)).toEqual(expected);
+  });
+
+  it("3 lines", () => {
+    const expected = "  *  \n" + " *** \n" + "*****";
+    expect(tree("*", 3)).toEqual(expected);
+  });
+
+  it("4 lines", () => {
+    const expected = "   *   \n" + "  ***  \n" + " ***** \n" + "*******";
+    expect(tree("*", 4)).toEqual(expected);
   });
 });
